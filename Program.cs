330137a6d1bbc37d3c9dd9666ca14844d0b9ede9
Dhanya S V <dhanyavirupaksha@gmail.com>
2020-10-23 19:03:33 +0530

    [Dhanya]Add . Added  Welcome Message

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
 namespace SnakeAndLadderSimulator
 {
     class Program
-    {
+    {        
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Snake And Ladder Simulator");
