4e775d0592df79c6703cd759f4f0c1a6dcf6e59b
Dhanya S V <dhanyavirupaksha@gmail.com>
2020-10-23 19:25:44 +0530

    [Dhanya]Add .  Added comments

--- Program.cs
+++ Program.cs
@@ -3,7 +3,9 @@
 namespace SnakeAndLadderSimulator
 {
     class Program
-    {        
+    {
+        /// <summary>Defines the entry point of the application.</summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Snake And Ladder Simulator");
