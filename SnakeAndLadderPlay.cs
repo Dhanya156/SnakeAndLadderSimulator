5ab0ca104e30abf31e4205b9222d8b0cfc38b067
Dhanya S V <dhanyavirupaksha@gmail.com>
2020-10-25 12:59:33 +0530

    [Dhanya]Add . Added Game Between Two Player And Who Won The Play

--- SnakeAndLadderPlay.cs
+++ SnakeAndLadderPlay.cs
@@ -10,21 +10,26 @@ namespace SnakeAndLadderSimulator
         /// <summary>Creating a public variables</summary>
         public int currentPosition = 0;
         public int finalPosition = 0;
-        public int numOfTimeDieRoll = 1;
+        public int numOfTimeDieRoll = -1;
         public int dieNumber;
+        public int playerId;
+        /// <summary>Initializes a new instance of the <see cref="SnakeAndLadderPlay" /> class.</summary>
+        /// <param name="playerId">The player identifier.</param>
+        public SnakeAndLadderPlay(int playerId)
+        {
+            this.playerId = playerId;
+        }
         /// <summary>Method to Roll a die.</summary>
-        public void RollingADie()
+        public int RollingADie()
         {
            ///Random number generated for Die from 1 to 6
             Random random = new Random();
             dieNumber = random.Next(1, 7);
+            return dieNumber;
         }
 
         /// Checks for option whether it is noplay, ladder or snake
-        /// If noPlay player Stays at current position
-        /// If Ladder move from current position to final position based on dieNumber
-        /// If Snake move back from current position to final position based on dieNumber
-       
+        
         public void CheckForOption()
         {
             ///Random number to check Option
@@ -33,11 +38,15 @@ namespace SnakeAndLadderSimulator
             switch (option)
             {
                 case 0:
+                    /// If noPlay player Stays at current position
                     Console.WriteLine("No Play, Current Position: " + currentPosition);
                     break;
+                
                 case 1:
+                    /// If Ladder move from current position to final position based on dieNumber
                     finalPosition = currentPosition + dieNumber;
-                    Console.WriteLine("Ladder, current Position: " + currentPosition + ", Die Rolled: " + dieNumber + ", Final Position: " + finalPosition);
+                    Console.WriteLine("Ladder, current Position: " + currentPosition + ", Die Rolled: " + dieNumber + ", Final Position: " + finalPosition);              
+                    ///If player finalposition exceds 50, player stays at current position
                     if (finalPosition > 50)
                     {
                         finalPosition = currentPosition;
@@ -48,11 +57,15 @@ namespace SnakeAndLadderSimulator
                         currentPosition = finalPosition;
                         Console.WriteLine("The Player at position" + currentPosition);
                     }
+                    ///If player gets ladder one more chance to play
+                    RollingADie();
                     break;
                 default:
+                    /// If Snake move back from current position to final position based on dieNumber
                     finalPosition = currentPosition - dieNumber;
                     Console.WriteLine("Snake, current Position: " + currentPosition + ", Die Number: " + dieNumber );
                     currentPosition = finalPosition;
+                    ///when player gets snake reaches 0 again he starts from 0
                     if (currentPosition < 0)
                     {
                         currentPosition = 0;
@@ -63,24 +76,6 @@ namespace SnakeAndLadderSimulator
                     break;      
             }
         }
-        /// <summary>Winning position of player at 50</summary>
-        public void WinningPosition()
-        {
-            while(numOfTimeDieRoll>0)
-            {
-                if(finalPosition==50)
-                {
-                    Console.WriteLine("player reached winning position");
-                    break;
-                }
-                else
-                {
-                    RollingADie();
-                    CheckForOption();   
-                }
-                numOfTimeDieRoll++;
-                Console.WriteLine("The Number Of Times Di Rolled To Reach Winning Position" +numOfTimeDieRoll);
-            }
-        }
+        
     }
 }
